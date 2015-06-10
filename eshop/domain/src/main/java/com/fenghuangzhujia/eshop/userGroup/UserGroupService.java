cc4703c77a4ede5393f71cd2247087261ac63a4d
TianmingYi <yzdytm@163.com>
2015-06-10 14:05:52 +0800

    批量添加分组

--- eshop/domain/src/main/java/com/fenghuangzhujia/eshop/userGroup/UserGroupService.java
+++ eshop/domain/src/main/java/com/fenghuangzhujia/eshop/userGroup/UserGroupService.java
@@ -1,5 +1,8 @@
 package com.fenghuangzhujia.eshop.userGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +18,25 @@ public class UserGroupService extends DtoPagingService<UserGroup, UserGroupDto, 
 	public UserGroupDto inGroup(long expVal) {
 		UserGroup group=getRepository().inGroup(expVal);
 		return adapter.convert(group);
+	}
+	
+	/**
+	 * 添加全部分组信息，删除已有信息
+	 * @param list
+	 * @return
+	 */
+	public List<UserGroupDto> addAll(Iterable<UserGroupInputArgs> list) {
+		//删除已有全部分组信息
+		getRepository().deleteAll();
+		//保存新分组
+		List<UserGroup> groups=new ArrayList<UserGroup>();
+		for (UserGroupInputArgs inputArgs : list) {
+			UserGroup group=adapter.convertToDo(inputArgs);
+			groups.add(group);
+		}
+		Iterable<UserGroup> resultGroups=getRepository().save(groups);
+		List<UserGroupDto> result=adapter.convertDoList(resultGroups);
+		return result;
 	}
 	
 	@Autowired
--- eshop/web/src/main/java/com/fenghuangzhujia/eshop/web/controller/admin/UserGroupController.java
+++ eshop/web/src/main/java/com/fenghuangzhujia/eshop/web/controller/admin/UserGroupController.java
@@ -1,13 +1,17 @@
 package com.fenghuangzhujia.eshop.web.controller.admin;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fenghuangzhujia.eshop.userGroup.UserGroupService;
 import com.fenghuangzhujia.eshop.userGroup.dto.UserGroupDto;
 import com.fenghuangzhujia.eshop.userGroup.dto.UserGroupInputArgs;
 import com.fenghuangzhujia.foundation.core.rest.PagingController;
+import com.fenghuangzhujia.foundation.core.rest.RequestResult;
 
 @RestController
 @RequestMapping("admin/userGroup")
@@ -19,5 +23,23 @@ public class UserGroupController extends PagingController<UserGroupDto, UserGrou
 	@Override
 	public UserGroupService getService() {
 		return service;
+	}
+	
+	@RequestMapping(value="addAll",method=RequestMethod.POST)
+	public String addAll(UserGroupList list) {
+		List<UserGroupDto> result=getService().addAll(list.getList());
+		return RequestResult.success(result).toJson();
+	}
+	
+	public static class UserGroupList {
+		private List<UserGroupInputArgs> list;
+
+		public List<UserGroupInputArgs> getList() {
+			return list;
+		}
+
+		public void setList(List<UserGroupInputArgs> list) {
+			this.list = list;
+		}
 	}
 }
