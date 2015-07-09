e7606efa90d0f5ee76fe3fa0a7855316be438e85
TianmingYi <yzdytm@163.com>
2015-07-09 17:35:14 +0800

    添加前端获取主材接口。

---
+++ eshop/web/src/main/java/com/fenghuangzhujia/eshop/web/controller/api/MeteriaController.java
@@ -0,0 +1,25 @@
+package com.fenghuangzhujia.eshop.web.controller.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fenghuangzhujia.eshop.meteriaManage.brand.BrandService;
+import com.fenghuangzhujia.eshop.meteriaManage.brand.dto.BrandVo;
+import com.fenghuangzhujia.foundation.core.rest.RequestResult;
+
+@RestController
+public class MeteriaController {
+	
+	@Autowired
+	private BrandService brandService;
+
+	@RequestMapping(value="meterias",method=RequestMethod.GET)
+	public String meterias() {
+		List<BrandVo> result=brandService.getDetailedBrands();
+		return RequestResult.success(result).toJson();
+	}
+}
