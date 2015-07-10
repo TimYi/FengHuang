6b7605ef47f158b155ae64443d2e936b8581b7b5
TianmingYi <yzdytm@163.com>
2015-07-10 09:50:39 +0800

    添加排序接口

--- eshop/web/src/main/java/com/fenghuangzhujia/eshop/web/controller/admin/BrandController.java
+++ eshop/web/src/main/java/com/fenghuangzhujia/eshop/web/controller/admin/BrandController.java
@@ -2,11 +2,13 @@ package com.fenghuangzhujia.eshop.web.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fenghuangzhujia.eshop.meteriaManage.brand.BrandService;
 import com.fenghuangzhujia.eshop.meteriaManage.brand.dto.BrandDto;
 import com.fenghuangzhujia.eshop.meteriaManage.brand.dto.BrandInputArgs;
+import com.fenghuangzhujia.foundation.core.rest.RequestResult;
 import com.fenghuangzhujia.foundation.core.rest.SpecificationController;
 
 @RestController(value="adminBrandController")
@@ -19,5 +21,11 @@ public class BrandController extends SpecificationController<BrandDto, BrandInpu
 	@Override
 	public BrandService getService() {
 		return service;
+	}
+	
+	@RequestMapping(value="order",method=RequestMethod.POST)
+	public String order(String[] ids) {
+		getService().reOrder(ids);
+		return RequestResult.success("排序完成").toJson();
 	}
 }
