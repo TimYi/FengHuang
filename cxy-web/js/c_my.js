5fa8835c0e4a4db0e7f48655f55180c7e4aa9616
chenxy <chenxuanyu@xiaocheben.com>
2015-07-12 12:16:30 +0800

    1.上传头像

--- cxy-web/js/c_my.js
+++ cxy-web/js/c_my.js
@@ -34,7 +34,7 @@ $(function(){
 	$("#bindbtn").bind("click",bindPhone);
 
 	//头像
-	//$("#avatarbtn").bind("click",avatarBtnUp);
+	$("#avatarbtn").bind("click",avatarBtnUp);
 
 	//安全退出
 	function loginOut(){
@@ -229,6 +229,10 @@ $(function(){
 	//修改个人资料
 	function setUserInfoHtml(data){
 		var obj = data.user || {};
+		var avatar = obj.avatar || "";
+		if(avatar !== "" ){
+			avatar = avatar.url || "";
+		}
 		var nikeName = obj.cnname || "";
 		var validName = obj.realName || "";
 		var eName = obj.ename || "";
@@ -252,7 +256,9 @@ $(function(){
 
 		//获取验证码
 		//getImgCode();
-
+		if(avatar !== ""){
+			$("#avatarbtn img").attr("src",avatar);
+		}
 		//昵称
 		$("#nikename").val(nikeName);
 		//真实姓名
@@ -310,7 +316,7 @@ $(function(){
 		$("#logintime").html(li.join(''));
 
 		//setUserFunHtml();
-
+		$("#loginoutbtn").show();
 
 	}
 
@@ -505,11 +511,12 @@ $(function(){
 		var popbox = $("#popbox");
 		if(popbox.length == 0){
 			var url = Base.serverUrl + "/api/user/changeAvatar";
+			//var url = "http://192.168.10.209:8080/fenghuangzhujia-eshop-web/";
 			var token = g.token;
 			var html = [];
 			html.push('<div id="popbox" class="prompt_mask transparentbg" style="display: block;">');
 			html.push('<div class="p_load" style="width:600px;height:200px;background:#fff;margin-left:-300px;">');
-			html.push('<form id="avatarform" action="' + url + '" method="post" enctype="multipart/form-data">');
+			html.push('<form id="avatarform" submit="return false;" action="' + url + '" method="post" enctype="multipart/form-data">');
 			html.push('<p>');
 			html.push('<input id="avatar" type="file" name="avatar" multiple="multiple" min="1" max="99" value="选择头像" />');
 			//html.push('<input id="uploadbtn" type="submit" value="upload" />');
@@ -530,6 +537,39 @@ $(function(){
 	}
 
 	function uploadBtnUp(){
+		var url = Base.serverUrl + "/api/user/changeAvatar";
+		var condi = {};
+		condi.token = g.token;
+		 $.ajaxFileUpload
+            (
+                {
+                    url: url, //用于文件上传的服务器端请求地址
+                    data:condi,
+                    secureuri: false, //一般设置为false
+                    fileElementId: 'avatar', //文件上传空间的id属性  <input type="file" id="file" name="file" />
+                    dataType: 'json', //返回值类型 一般设置为json
+                    success: function (data, status)  //服务器成功响应处理函数
+                    {
+			//debugger
+                        $("#img1").attr("src", data.imgurl);
+                        if (typeof (data.error) != 'undefined') {
+                            if (data.error != '') {
+                                alert(data.error);
+                            } else {
+                                alert(data.msg);
+                            }
+                        }
+                    },
+                    error: function (data, status, e)//服务器响应失败处理函数
+                    {
+                        alert(e);
+                    }
+                }
+            )
+            return false;
+
+
+		return;
 		var url = Base.serverUrl + "/api/user/changeAvatar";
 		var token = g.token;
 		var xhr = new XMLHttpRequest();
