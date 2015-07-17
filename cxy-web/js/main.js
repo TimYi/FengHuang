5afeb4bc30e704e2e61f474fcbf35f2f73cbe611
chenxy <chenxuanyu@xiaocheben.com>
2015-07-17 17:53:52 +0800

    1.修改首页抢购

--- cxy-web/js/main.js
+++ cxy-web/js/main.js
@@ -85,6 +85,8 @@ $(function(){
 	g.totalPage = 1;
 	g.currentPage = 1;
 	g.paseSize = 20;
+	//标识是否抢购成功
+	g.hasbuy = false;
 	g.httpTip = new Utils.httpTip({});
 	g.listdata = [];
 	g.userprofile = Utils.offLineStore.get("login_userprofile",false) || "";
@@ -727,6 +729,11 @@ $(function(){
 	}
 
 	function miaoSha(id){
+		if(g.hasbuy){
+			//直接跳订单
+			location.href = "center/c_order.html?token=" + g.token + "&p=7";
+			return;
+		}
 		var url = Base.scramble;
 		var condi = {};
 		condi.token = g.token;
@@ -744,6 +751,7 @@ $(function(){
 				console.log("miaoSha",data);
 				var status = data.status || "";
 				if(status == "OK"){
+					g.hasbuy = true;
 					alert("抢购成功");
 					//Utils.alert("抢购成功");
 					//var orderId = data.result.id;
