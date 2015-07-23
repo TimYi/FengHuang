a2461a43132f44918c8ec6859209b8db4a75d5b8
zhangqian <511329078@qq.com>
2015-07-23 23:58:26 +0800

    套餐详情

---
+++ zq-webapp/js/tc_detail.js
@@ -0,0 +1,148 @@
+$(function() {
+    var g = {};
+    g.phone = "";
+    g.imgCodeId = "";
+    g.sendCode = false;
+    g.sendTime = 60;
+    g.username = Base.userName;
+    g.token = Utils.offLineStore.get("token", false);
+    g.packageId = Utils.getQueryString("packageId");
+    g.totalPage = 1;
+    g.currentPage = 1;
+    g.paseSize = 10;
+    g.httpTip = new Utils.httpTip({});
+    g.listdata = [];
+    //验证登录状态
+    g.loginStatus = Utils.getUserInfo();
+
+    getPackageDetail();
+    getPackageMeterial();
+
+    function changeBtn(data){
+        var obj = data || '';
+        var $btn = $('#btn-buy');
+        if(obj.hasScrambled){
+            $btn.find("div").text("已抢购");
+        }else if(obj.hasAppointed){
+            $btn.find("div").text("立刻抢购");
+            $btn.attr('href','javascript:miaoSha(\'' + g.packageId + '\')');
+        }else{
+            $btn.find("div").text("立刻预约");
+            $btn.attr("href","subcheck.html?id="+g.packageId);
+        }
+    }
+
+    function getPackageDetail(){
+        var url = Base.packageDetail+'/'+g.packageId;
+        var condi = {};
+        condi.id = g.packageId;
+        g.httpTip.show();
+        $.ajax({
+            url:url,
+            data:condi,
+            type:"GET",
+            dataType:"json",
+            context:this,
+            global:false,
+            success: function(data){
+                var status = data.status || "";
+                console.log(data);
+                if(status == "OK"){
+                    changeBtn(data.result);
+                }else{
+                    Utils.alert("套餐获取失败");
+                }
+                g.httpTip.hide();
+            },
+            error:function(data){
+                g.httpTip.hide();
+            }
+        });
+    }
+
+    function getPackageMeterial(){
+        var url = Base.serverUrl+'/api/product/package/'+ g.packageId +'/materials';
+        var condi = {};
+        condi.id = g.packageId;
+        g.httpTip.show();
+        $.ajax({
+            url:url,
+            data:condi,
+            type:"GET",
+            dataType:"json",
+            context:this,
+            global:false,
+            success: function(data){
+                var status = data.status || "";
+                if(status == "OK"){
+                    changeMeterialHtml(data.result);
+                }else{
+                    Utils.alert("套餐主材获取失败");
+                }
+                g.httpTip.hide();
+            },
+            error:function(data){
+                g.httpTip.hide();
+            }
+        });
+    }
+
+    function changeMeterialHtml(data){
+        var obj = data || '';
+        console.log(obj);
+        var html = [];
+        for(var key in obj){
+            var brand = key;
+            var mete = obj[key] || '';
+            var product = mete[0].product || '';
+            var name = product.name || '';
+            html.push('<span style="color:#000;font-size:14px;">'+ brand +' ／ '+ name +'</span>');
+            html.push('<ul data-am-widget="gallery" class="am-gallery am-avg-sm-4 am-gallery-default" data-am-gallery="{ pureview: true }">');
+            for(var i = 0,len=mete.length;i<len;i++){
+                var pic = mete[i].pic || '';
+                var picurl = pic.url || '';
+                html.push('<li><div class="am-gallery-item">');
+                html.push('<a href="'+ picurl +'" class="">');
+                html.push('<img class="am-thumbnail" src="'+ picurl +'" />');
+                html.push('</a></div></li>');
+            }
+            html.push('</ul>');
+        }
+        $('#meterialWrap').html(html.join(''));
+        $.AMUI.gallery.init();
+    }
+
+    function miaoSha(id){
+        var url = Base.scramble;
+        var condi = {};
+        condi.token = g.token;
+        condi.id = id;
+        condi.caseId = "";
+        g.httpTip.show();
+        $.ajax({
+            url:url,
+            data:condi,
+            type:"POST",
+            dataType:"json",
+            context:this,
+            global:false,
+            success: function(data){
+                console.log("miaoSha",data);
+                var status = data.status || "";
+                if(status == "OK"){
+                    var orderId = data.result.id;
+                    location.href = "paycheck.html?id=" + orderId;
+                }
+                else{
+                    Utils.alert("抢购失败");
+                }
+                g.httpTip.hide();
+            },
+            error:function(data){
+                g.httpTip.hide();
+            }
+        });
+    }
+
+    window.miaoSha = miaoSha;
+});
