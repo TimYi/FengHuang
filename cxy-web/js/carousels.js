ad1352093a6c6e910c522f9107fb220fb3f66643
chenxy <chenxuanyu@xiaocheben.com>
2015-07-23 12:45:15 +0800

    aa

--- cxy-web/js/carousels.js
+++ cxy-web/js/carousels.js
@@ -149,7 +149,7 @@ $(function(){
 	}
 	for(var i = 0,len = subbtn.length; i < len; i++){
 		var btn = $(subbtn[i]);
-		var href = btn.attr("href").tirm();;
+		var href = btn.attr("href");
 		if(href == "#TYG_XM_BTNTITLE#" || href == "#TYG_BJ_BTNTITLE#"){
 			//预约体验馆
 			if(loginStatus){
