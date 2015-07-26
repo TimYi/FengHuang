b4c6f246a01eb1f0c73a4409105012d51dbb7d9e
scosun <scosun@163.com>
2015-07-26 13:56:21 +0800

    1

--- cxy-web/js/699.js
+++ cxy-web/js/699.js
@@ -125,10 +125,10 @@ $(function(){
 					imgUrl = imgUrl.url || "";
 				}
 				var description =  imgarr[j].description || "";
-				html.push('<table><tr><td><img src="' + imgUrl + '" style="' + css + '">');
+				html.push('<table><tr><img src="' + imgUrl + '" style="' + css + '"></tr>');
 
 				//新增产品描述
-				html.push('<span style="font-size:12px;">' + description + "</span><td></tr></table>");
+				html.push('<tr><span style="font-size:12px;">' + description + "</span></tr></table>");
 			}
 
 			html.push('</td>');
