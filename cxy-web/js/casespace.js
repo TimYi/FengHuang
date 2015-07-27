29973deeaaac7596c6835e04b6f8e40f6377686d
chenxy <chenxuanyu@xiaocheben.com>
2015-07-27 21:05:24 +0800

    add

--- cxy-web/js/casespace.js
+++ cxy-web/js/casespace.js
@@ -160,9 +160,11 @@ $(function(){
 		//$("#featurelogo").css("visibility","visible");
 		//$("#featurepro").css("visibility","visible");
 
-		//$('#myTab a:last').tab('show');//初始化显示哪个tab
+
 
 		$(function(){
+			$('#myTab a:first').tab('show');//初始化显示哪个tab
+
 			$('#myTab a').click(function (e) {
 				e.preventDefault();//阻止a链接的跳转行为
 				$(this).tab('show');//显示当前选中的链接及关联的content
