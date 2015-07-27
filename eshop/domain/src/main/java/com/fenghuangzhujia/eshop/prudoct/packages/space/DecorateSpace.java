96a54349845aa7fc535d3f486856a2397b5fd6ec
TianmingYi <yzdytm@163.com>
2015-07-27 15:24:41 +0800

    提交装修空间实体

--- eshop/domain/src/main/java/com/fenghuangzhujia/eshop/prudoct/packages/space/DecorateSpace.java
+++ eshop/domain/src/main/java/com/fenghuangzhujia/eshop/prudoct/packages/space/DecorateSpace.java
@@ -2,14 +2,18 @@ package com.fenghuangzhujia.eshop.prudoct.packages.space;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import com.fenghuangzhujia.eshop.prudoct.packages.DecoratePackage;
 import com.fenghuangzhujia.foundation.core.entity.UUIDBaseModel;
 import com.fenghuangzhujia.foundation.media.MediaContent;
 
+@Entity
+@Table(name="fhzj_decorate_space")
 public class DecorateSpace extends UUIDBaseModel {
 
 	/**所属套餐*/
