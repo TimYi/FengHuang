cee39eed28749c9e5a56dc638dbd4b2c12c63171
chenxy <chenxuanyu@xiaocheben.com>
2015-07-28 10:11:41 +0800

    add

--- cxy-web/js/gyzs.js
+++ cxy-web/js/gyzs.js
@@ -2,37 +2,32 @@
 (function(){
 
 	function sendGetTechnologiesHttp(){
-		if(pageid !== ""){
-			var url = Base.serverUrl + "/api/technologies";
-			//g.httpTip.show();
-			$.ajax({
-				url:url,
-				data:{},
-				type:"GET",
-				dataType:"json",
-				context:this,
-				global:false,
-				async: false,
-				success: function(data){
-					console.log("sendGetTechnologiesHttp",data);
-					var status = data.status || "";
-					if(status == "OK"){
-						changeTechnologiesHtml(data);
-					}
-					else{
-						var msg = data.errorDescription || "";
-						Utils.alert("获取工艺展示错误:" + msg);
-					}
-					//g.httpTip.hide();
-				},
-				error:function(data){
-					//g.httpTip.hide();
+		var url = Base.serverUrl + "/api/technologies";
+		//g.httpTip.show();
+		$.ajax({
+			url:url,
+			data:{},
+			type:"GET",
+			dataType:"json",
+			context:this,
+			global:false,
+			async: false,
+			success: function(data){
+				console.log("sendGetTechnologiesHttp",data);
+				var status = data.status || "";
+				if(status == "OK"){
+					changeTechnologiesHtml(data);
 				}
-			});
-		}
-		else{
-			//alert("链接错误");
-		}
+				else{
+					var msg = data.errorDescription || "";
+					Utils.alert("获取工艺展示错误:" + msg);
+				}
+				//g.httpTip.hide();
+			},
+			error:function(data){
+				//g.httpTip.hide();
+			}
+		});
 	}
 
 
@@ -40,35 +35,43 @@
 		var obj = data.result || [];
 
 		var html = [];
-		html.push('<section style="border-top:1px solid #f6f6f6;">');
-		html.push('<div class="container"  style="margin-bottom:-50px">');
-		html.push('<div class="center wow fadeInDown">');
-		html.push('<h2>电路改造工艺<span style="font-size:24px"> FREE SERVICE</span></h2>');
-		html.push('<p class="lead"></p>');
-		html.push('</div>');
-		html.push('<div class="container">');
-		html.push('<div class="row" style="margin:-20px 0 30px 0">');
-		html.push('<div class="col-md-4">');
-		html.push('<div class="recent-work-wrap">');
-		html.push('<img class="img-responsive" src="assets/images/dl1.gif" alt="">');
-		html.push('</div>');
-		html.push('</div>');
+		for(var i = 0, len = obj.length; i < len; i++){
+			var name = obj[i].name || "";
+			var pic = obj[i].pic || "";
+			if(pic !== ""){
+				pic = pic.url || "";
+			}
+			var description = obj[i].description || "";
 
-		html.push('<div class="col-md-8">');
-		html.push('<div class="overlay">');
-		html.push('<div class="recent-work-inner">');
-		html.push('<p style="font-size:16px;">');
-		html.push('PVC线管线盒，暗管暗槽横平竖直，电线导管无折皱、无裂缝保证电线导管畅通。强弱电要分开，互不干扰很重要“左零右火中间零”安装面板要记牢.10A以上大功率电器，必须使用4mm的电源线设置专用供电回路，确保大功率电器稳定运行');
-		html.push('</p>');
-		html.push('</div>');
-		html.push('</div>');
-		html.push('</div>');
-		html.push('</div>');
-		html.push('</div>');
-		html.push('</div>');
-		html.push('</section>');
+			html.push('<section style="border-top:1px solid #f6f6f6;">');
+			html.push('<div class="container"  style="margin-bottom:-50px">');
+			html.push('<div class="center wow fadeInDown">');
+			html.push('<h2>' + name + '<span style="font-size:24px"> FREE SERVICE</span></h2>');
+			html.push('<p class="lead"></p>');
+			html.push('</div>');
+			html.push('<div class="container">');
+			html.push('<div class="row" style="margin:-20px 0 30px 0">');
+			html.push('<div class="col-md-4">');
+			html.push('<div class="recent-work-wrap">');
+			html.push('<img class="img-responsive" src="' + pic + '" alt="" style="width:350px;height:265px;">');
+			html.push('</div>');
+			html.push('</div>');
 
-		//document.write(html.join(''));
+			html.push('<div class="col-md-8">');
+			html.push('<div class="overlay">');
+			html.push('<div class="recent-work-inner">');
+			html.push('<p style="font-size:16px;">');
+			html.push(description);
+			html.push('</p>');
+			html.push('</div>');
+			html.push('</div>');
+			html.push('</div>');
+			html.push('</div>');
+			html.push('</div>');
+			html.push('</div>');
+			html.push('</section>');
+		}
+		document.write(html.join(''));
 	}
 
 	sendGetTechnologiesHttp();
