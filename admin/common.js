20b5c82eaeb98cb978695083c1a1b666e144ae03
feegle <fhm727@163.com>
2015-08-02 00:40:49 +0800

    新增体验馆管理功能

--- admin/common.js
+++ admin/common.js
@@ -8,6 +8,10 @@ var TEMPLATE_CAROUSEL = 'carousel';
 var TEMPLATE_TEMPLATE = 'template';
 var ORDER_DRAWBACK = 'drawback';
 var PACKAGE_SPACE = 'decorateSpace';
+var MUSEUM_MUSEUM = 'experienceMuseum';
+//var AREA_CITY = 'area/level/CITY';
+var AREA_CITY = 'area';
+
 function PagingJson2ViewModel(data){
 
 	return ko.mapping.fromJS(data);
