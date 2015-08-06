218569da44e5b4acc7bf736a0c2ddb691b569ba6
TianmingYi <yzdytm@163.com>
2015-08-06 16:43:55 +0800

    添加报名查询接口

--- eshop/domain/src/main/java/com/fenghuangzhujia/eshop/activity/signup/SignupService.java
+++ eshop/domain/src/main/java/com/fenghuangzhujia/eshop/activity/signup/SignupService.java
@@ -1,5 +1,7 @@
 package com.fenghuangzhujia.eshop.activity.signup;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,4 +11,12 @@ import com.fenghuangzhujia.foundation.core.dto.DtoPagingService;
 @Transactional
 public class SignupService extends DtoPagingService<Signup, Signup, Signup, String> {
 
+	public List<Signup> query(String telephone) {
+		return getRepository().query(telephone);
+	}
+	
+	@Override
+	public SignupRepository getRepository() {
+		return (SignupRepository)super.getRepository();
+	}
 }
--- eshop/web/src/main/java/com/fenghuangzhujia/eshop/web/controller/PageController.java
+++ eshop/web/src/main/java/com/fenghuangzhujia/eshop/web/controller/PageController.java
@@ -6,14 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fenghuangzhujia.eshop.activity.signup.Signup;
-import com.fenghuangzhujia.eshop.activity.signup.SignupService;
-import com.fenghuangzhujia.eshop.core.authentication.AuthenticationService;
-import com.fenghuangzhujia.eshop.core.authentication.SimpleUserDetails;
 import com.fenghuangzhujia.eshop.decorateProcess.DecorateProcessService;
 import com.fenghuangzhujia.eshop.decorateProcess.dto.DecorateProcessDto;
-import com.fenghuangzhujia.eshop.message.MessageService;
-import com.fenghuangzhujia.eshop.message.dto.MessageInputArgs;
 import com.fenghuangzhujia.eshop.view.carousel.CarouselService;
 import com.fenghuangzhujia.eshop.view.carousel.dto.CarouselDto;
 import com.fenghuangzhujia.eshop.view.decorateTechnology.DecorateTechnologyService;
@@ -33,10 +27,6 @@ public class PageController {
 	private DecorateProcessService decorateProcessService;
 	@Autowired
 	private DecorateTechnologyService technologyService;
-	@Autowired
-	private SignupService signupService;
-	@Autowired
-	private MessageService messageService;
 
 	@RequestMapping(value="navigations",method=RequestMethod.GET)
 	public String navagations() {
@@ -60,22 +50,5 @@ public class PageController {
 	public String technologies() {
 		Iterable<DecorateTechnologyDto> result=technologyService.findAll();
 		return RequestResult.success(result).toJson();
-	}
-	
-	@RequestMapping(value="signup",method=RequestMethod.POST)
-	public String signup(Signup signup) {	
-		signupService.add(signup);
-		
-		//如果用户已经登录，发送留言
-		if(AuthenticationService.isLogin()) {
-			SimpleUserDetails details=AuthenticationService.getUserDetail();
-			String userId=details.getId();
-			MessageInputArgs args=new MessageInputArgs();
-			args.setUserid(userId);
-			args.setTitle("情歌对唱报名成功");
-			args.setContent("您已成功报名凤凰筑家“为爱而唱，为家而唱”情歌对唱赛！");
-			messageService.add(args);
-		}				
-		return RequestResult.success("报名成功").toJson();
 	}
 }
