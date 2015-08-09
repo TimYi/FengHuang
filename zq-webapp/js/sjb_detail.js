882fe7beb36745965e259d54942325fe7da7720e
scosun <scosun@163.com>
2015-08-09 16:26:11 +0800

    cd \d

--- zq-webapp/js/sjb_detail.js
+++ zq-webapp/js/sjb_detail.js
@@ -84,7 +84,7 @@ $(function() {
             var mete = obj[key] || '';
             var product = mete[0].product || '';
             var name = product.name || '';
-            html.push('<span style="color:#000;font-size:14px;">'+ brand +' ／ '+ name +'</span>');
+            html.push('<hr/><span style="color:#000;font-size:14px;font-weight:800">'+ brand +'</span> - <span style="color:#666;font-size:14px;"> '+ name +'</span>');
             html.push('<ul data-am-widget="gallery" class="am-gallery am-avg-sm-4 am-gallery-default" data-am-gallery="{ pureview: true }">');
             for(var i = 0,len=mete.length;i<len;i++){
                 var pic = mete[i].pic || '';
