03d2869b4d9d9a598333ca24b9e328a08025b4d4
TianmingYi <yzdytm@163.com>
2015-08-27 11:11:18 +0800

    使用更友好的错误提示

--- eshop/core/src/main/java/com/fenghuangzhujia/eshop/core/validate/core/exception/NotFoundException.java
+++ eshop/core/src/main/java/com/fenghuangzhujia/eshop/core/validate/core/exception/NotFoundException.java
@@ -4,7 +4,7 @@ public class NotFoundException extends ValidateException {
 
 	private static final long serialVersionUID = -763348787653582512L;
 	
-	private static final String message="凭据不存在";
+	private static final String message="验证码错误";
 	
 	public NotFoundException(){
 		super(message);
